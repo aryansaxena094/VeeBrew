77696e3c2f8619eb9596685f755194a714e993ad
Aryan <aryan.saxena094@gmail.com>
2023-03-22 19:11:43 -0400

    commit

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const bodyParser = require("body-parser");
 const portNumber = "3999";
 
 const app = express();
+app.use(express.static("public"));
 
 app.listen(portNumber, function(){
     console.log("Server is running on port: "+portNumber);
+});
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/public/signup.html");
 });
