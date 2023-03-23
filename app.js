537a04ffd59ff4a5c4d30aa9a354f841b1dc07ae
Aryan <aryan.saxena094@gmail.com>
2023-03-22 20:16:09 -0400

    commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Requires 
+//Requires
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
@@ -8,6 +8,7 @@ const portNumber = "3999";
 
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(portNumber, function(){
     console.log("Server is running on port: "+portNumber);
@@ -16,3 +17,27 @@ app.listen(portNumber, function(){
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/public/signup.html");
 });
+
+app.post("/", function(req, res){
+    const firstname = req.body.fname;
+    const lastname = req.body.lname;
+    const email = req.body.email;
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribe",
+                merge_fields: {
+                    FNAME: firstname,
+                    LNAME: lastname
+                }
+            }
+        ]
+    };
+    var jsondata = JSON.stringify(data);
+});
+
+//APIKEY
+// 1f76428a37956f3c0b906f94fa9b507e-us21
+//LISTID
+// 1692f84714
